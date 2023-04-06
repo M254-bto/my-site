53adddda81db611c9e08bbe8d34a733278eb8671
M254-bto <ngechamike26@gmail.com>
2023-04-07 00:03:15 +0300

    projects

--- blogs.py
+++ blogs.py
@@ -6,3 +6,7 @@ def blogs():
         ##### Ive been out and about
         """
     )
+
+    st.write("Here's my Medium article:")
+    medium_url = "https://medium.com/@ngechamike26/advanced-python-concepts-27d7d42f276e"
+    st.components.v1.iframe(medium_url, height=600, scrolling=True)
--- projects.py
+++ projects.py
@@ -3,7 +3,5 @@ import streamlit as st
 
 def projects():
     st.write(
-        """
-        ##### What I'm i up to, you ask;
-        """
+        
     )
